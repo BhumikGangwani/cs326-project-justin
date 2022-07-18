171b3b3a1edf15aabb466fe68cec7d7c4e36fc9f
Bhumik Gangwani <gangwanibhumik@gmail.com>
2022-07-19 02:06:34 +0530

    commented the code and added milestone-2.md

---
+++ docs/milestone-2.md
@@ -0,0 +1,3 @@
+* For the initial part of the web page, the interactivity is limited to the count of likes and dislikes for a food location.
+* Reviews and filter will be added in the next part of the project.
+* Link to video of application: https://www.loom.com/share/5e39116b1e3b4715a2bfd065e776c407
--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+// UI Components
+// Grab the DOM elements that I work with
 const upvoteButton = document.getElementById('upvote');
 const downvoteButton = document.getElementById('downvote');
 const upvoteCountElement = document.getElementById('upvote-count');
 const downvoteCountElement = document.getElementById('downvote-count');
 
+// Keeps track of the users vote
 let upvoteButtonClicked = false;
 let downvoteButtonClicked = false;
 
+// Change the vote images to reflect that the user has clicked upvote
+// Increase the count of upvotes by 1
+// Decrease the count of downvotes by 1 if the user had previously downvoted the food location
 upvoteButton.addEventListener('click', () => {
   if (!upvoteButtonClicked) {
     upvoteButton.src = "images/clicked-upvote.webp";
@@ -19,6 +25,9 @@ upvoteButton.addEventListener('click', () => {
   }
 });
 
+// Change the vote images to reflect that the user has clicked downvote
+// Increase the count of downvotes by 1
+// Decrease the count of upvotes by 1 if the user had previously upvoted the food location
 downvoteButton.addEventListener('click', () => {
   if (!downvoteButtonClicked) {
     downvoteButton.src = "images/clicked-downvote.png";
